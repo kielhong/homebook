fe991b98058c99f545201ced4cfe1b4499566fc2
kielhong <kielhong@sk.com>
2015-11-28 15:40:41 +0900

    init

---
+++ src/main/java/net/kiel/HomebookApplication.java
@@ -0,0 +1,12 @@
+package net.kiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomebookApplication.class, args);
+    }
+}
---
+++ src/main/python/crawal_book.py
@@ -0,0 +1,10 @@
+__author__ = 'kiel'
+
+import re
+
+
+# ---- read html (http://www.yes24.com/24/Goods/20520429) ----
+# title : 누구나 쉽게 배우는 코딩
+# publisher : 비제이퍼블릭(BJ퍼블릭)
+# publish date : 2015년 10월 07일
+# isbn-13 : 9791186697047
